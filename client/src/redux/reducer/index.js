47ae3c2b12807fee03b9e3400b8fa7da0b8dbe25
francedermaz <francedermaz@gmail.com>
2022-03-25 19:56:54 -0300

    reducer login,create,logout

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -1,13 +1,37 @@
 import * as actions from '../actions/index';
 
 const initialState = {
-
+    user:{},
+    loggedIn:false,
 }
 
 const rootReducer = (state=initialState,action)=>{
     switch(action.type){
-        case actions.GET_STH: return{
-        }
+        case "LOGIN_USER":
+            if (action.payload.token) {
+                const token = action.payload.token;
+                const useraux = jwt.decode(token);
+                const obj = useraux.user;
+                const user = {
+                    id: obj.id,
+                    email: obj.email,
+                    name: obj.name,
+                    lastName: obj.lastName,
+                };
+                localStorage.setItem("token", token);
+                localStorage.setItem("user", JSON.stringify(user));
+                return { ...state, user: user, loggedIn: true };
+            } 
+            else{
+              return { ...state, user: {}, loggedIn: false };
+            }
+        case "LOG_OUT_USER":
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            localStorage.removeItem("cartProducts");
+            return { ...state, user: {}, loggedIn: false };
+          case "CREATE_USER":
+            return { ...state };
         default: return state;
     }
 }
