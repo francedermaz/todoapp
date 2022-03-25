391f07c83586ff956d20b1c71f6a6d66301cc011
francedermaz <francedermaz@gmail.com>
2022-03-25 15:36:05 -0300

    add routes index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,12 +2,16 @@ const { Router } = require('express');
 
 //
 const authRoute = require("./auth.js");
+const folderRoute = require("./folder.js");
+const itemRoute = require("./item.js");
 //
 
 const router = Router();
 
 //
 router.use("/auth", authRoute);
+router.use("/folder",folderRoute);
+router.use("/item",itemRoute);
 //
 
 module.exports = router;
