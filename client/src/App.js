ea2b4dd52605bd076b65e07683e4c47d41f7ed5f
francedermaz <francedermaz@gmail.com>
2022-03-25 20:25:49 -0300

    add contact

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Routes ,Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
+import Contact from './components/Contact/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path='/' element={<Landing/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/contact' element={<Contact/>}/>
       </Routes>
     </div>
   );
