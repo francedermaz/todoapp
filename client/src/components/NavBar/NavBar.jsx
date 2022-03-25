42a6e349a47edc89c350ffeee618fb6c40469b39
francedermaz <francedermaz@gmail.com>
2022-03-25 19:44:50 -0300

    navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,38 @@
+import { Link } from 'react-router-dom';
 import styles from './NavBar.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const NavBar = () => {
+const NavBar = ({is}) => {
     return(
-        <div></div>
+        <div>
+            <section className={styles.navbar}>
+                <Link to={'/'}>
+                    {
+                        <h1 className={styles.logo}>todoit</h1>
+                    }
+                </Link>
+                <div className={styles.menu}>
+                    <Link to={'/about'}>
+                        {
+                            is==='about'?<h1 className={styles.titleglow}>About</h1>:<h1 className={styles.title}>About</h1>
+                        }
+                    </Link>
+                    <Link to={'/contact'}>
+                        {
+                            is==='contact'?<h1 className={styles.titleglow}>Contact</h1>:<h1 className={styles.title}>Contact</h1>
+                        }
+                    </Link>
+                    <div className={styles.divlogin}>
+                    <Link to={'/login'}>
+                    {
+                        <FontAwesomeIcon className={styles.user} icon={faUser} />
+                    }
+                    </Link>
+                    </div>
+                </div>
+            </section>
+        </div>
     )
 }
 
