5f76de7457a5785c957329f2ad45e5af31cad6cb
francedermaz <francedermaz@gmail.com>
2022-03-25 19:51:20 -0300

    add login in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes ,Route } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<Landing/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </div>
   );
