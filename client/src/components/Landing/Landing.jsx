a25f943bfc77340ad64630644fd7788c1508ddae
francedermaz <francedermaz@gmail.com>
2022-03-25 19:45:11 -0300

    Landing Page

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,8 +1,21 @@
+import NavBar from '../NavBar/NavBar';
 import styles from './Landing.module.css';
+import tree from './assets/tree.png';
+import {Link} from 'react-router-dom';
 
 const Landing = () => {
     return(
-        <div></div>
+        <div>
+            <NavBar/>
+            <div className={styles.text}>
+                <img className={styles.img} src={tree} alt="tree"/>
+                <h1 className={styles.first}>Organize it all</h1>
+                <h1 className={styles.second}>with Todoit</h1>
+                <Link to="/login">
+                    <button className={styles.button}>Get started</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
