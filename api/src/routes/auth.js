f5d29290338de892161ac14a27b24c3c5ecf42f4
francedermaz <francedermaz@gmail.com>
2022-03-25 16:00:55 -0300

    get all users

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,8 +1,16 @@
 const { Router } = require('express');
 const { signUp, signIn } = require('../controllers/authController');
+const {User} = require('../db.js');
 
 const router = Router();
 
+router.get("/", async (req,res)=>{
+    User.findAll()
+    .then(data=>{
+        res.json(data);
+    })
+    .catch(e=>{return e;})
+});
 router.post('/signUp', signUp);
 router.post('/signIn', signIn);
 
