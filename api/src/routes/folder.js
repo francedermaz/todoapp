ee03e559f705a9cf624741297a17334e0b1de193
francedermaz <francedermaz@gmail.com>
2022-03-25 11:59:02 -0300

    get folders of a user

--- api/src/routes/folder.js
+++ api/src/routes/folder.js
@@ -13,5 +13,18 @@ router.get('/',async(req,res)=>{
     }
 })
 
+// Get All Folders of a User
+router.get('/:id',async(req,res)=>{
+    try{
+        const folder = await Folder.findAll({
+            where:{id:req.params.id}
+        });
+        res.status(200).send(folder);
+    }
+    catch(e){
+        res.status(500).send(e);
+    }
+})
+
 
 module.exports = router;
