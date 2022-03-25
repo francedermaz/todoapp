7d660ef9979387ef60e679afc1f2c4dafea62eb1
francedermaz <francedermaz@gmail.com>
2022-03-25 19:56:44 -0300

    actions create, login, logout

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,13 +1,29 @@
 import axios from 'axios';
 
-export const GET_STH = 'GET STH';
-
-export const getAll = () => {
-    return async function (dispatch){
-        let aux = await axios.get("http://localhost:3001/")
+export function loginUser(payload) {
+    return async function (dispatch) {
+        let req = await axios.post("http://localhost:3001/auth/signIn/",payload);
         return dispatch({
-            type: GET_STH,
-            payload: aux.data,
-        })
-    }
+            type: "LOGIN_USER",
+            payload: req.data,
+        });
+    };
+}
+  
+export function logoutUser() {
+    return async function (dispatch) {
+        return dispatch({
+            type: "LOG_OUT_USER",
+        });
+    };
+}
+  
+export function createUser(payload) {
+    return async function (dispatch) {
+        let req = await axios.post("http://localhost:3001/auth/signUp",payload);
+        return dispatch({
+            type: "CREATE_USER",
+            payload: req.data,
+        });
+    };
 }
