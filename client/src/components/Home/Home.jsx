01694b79ac90d5683b3d992b71528ace44dc958f
francedermaz <francedermaz@gmail.com>
2022-03-26 12:23:57 -0300

    add form

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import NavBar from '../NavBar/NavBar';
 import styles from './Home.module.css';
 import { useSelector, useDispatch } from "react-redux";
@@ -15,6 +15,27 @@ const Home = () => {
     let foldernum=null;
     const items = useSelector(state=>state.items);
     const folders = useSelector(state=>state.folders);
+    const [input,setInput] = useState({
+        name:''
+    })
+
+    function handleFolderClick(e){
+        foldernum=e.target.value;
+        dispatch(getItems(aux.id,foldernum));
+        dispatch(getFolders(0));
+    }
+
+    function handleChange(e){
+        e.preventDefault();
+        setInput({
+            ...input,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    function handleSubmit(e){
+        dispatch()
+    }
 
     useEffect(()=>{
         dispatch(getFolders(aux.id)); //Get folders
@@ -24,7 +45,12 @@ const Home = () => {
     return(
         <div>
             <NavBar/>
-            <div>
+            <div className={styles.page}>
+                <form onSubmit={e=>handleSubmit(e)}>
+                    <input className={styles.input}
+                    value={input.name} type='text' name='name' placeholder="Item" onChange={e=>handleChange(e)}>
+                    </input>
+                </form>
                 <ul>
                 {
                     items?.map(el=>{
@@ -35,7 +61,7 @@ const Home = () => {
                 <ul>
                 {
                     folders?.map(el=>{
-                        return <li>{el.name}</li>
+                        return <li id={el.id} value={el.id} onClick={(e)=>handleFolderClick(e)}>{el.name}</li>
                     })
                 }
                 </ul>
