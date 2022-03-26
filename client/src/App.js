1b31f310dedea9bd423f296e8169b69674f89765
francedermaz <francedermaz@gmail.com>
2022-03-26 19:06:51 -0300

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
 import Contact from './components/Contact/Contact';
 import Home from './components/Home/Home';
+import Folder from './components/Folder/Folder';
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         <Route path='/signup' element={<SignUp/>}/>
         <Route path='/contact' element={<Contact/>}/>
         <Route path='/home' element={<Home/>}/>
+        <Route path="/folder/:id" element={<Folder/>} />
+        <Route path="/item/:id" element={<Edit/>} />
+        <Route path="/folder/:id" element={<Create/>} />
       </Routes>
     </div>
   );
