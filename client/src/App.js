e7333745e1ebf11f61582f7632af0dc7fc10c349
francedermaz <francedermaz@gmail.com>
2022-03-27 00:42:32 -0300

    routes protected

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes ,Route } from 'react-router-dom';
+import { Routes ,Route, Navigate } from 'react-router-dom';
 import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
 import SignUp from './components/SignUp/SignUp';
@@ -12,18 +12,23 @@ import Account from './components/Account/Account';
 import Page404 from './components/Page404/Page404';
 
 function App() {
+  let user = { id: -1 };
+  if (localStorage.getItem("user")) {
+    user = localStorage.getItem("user");
+    user = JSON.parse(user);
+  }
   return (
     <div className="App">
       <Routes>
         <Route exact path='/' element={<Landing/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={user.id===-1?<Login/>:<Navigate to="/account"/>}/>
+        <Route path='/signup' element={user.id===-1?<SignUp/>:<Navigate to="/account"/>}/>
         <Route path='/contact' element={<Contact/>}/>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/home' element={user.id===-1?<Navigate to="/login"/>:<Home/>}/>
         <Route path="/folder/:id" element={<Folder/>} />
         <Route path="/edit/item/:id" element={<Edit/>} />
-        <Route path="/folder/create/" element={<Create/>} />
-        <Route path="/account" element={<Account/>} />
+        <Route path="/folder/create/" element={user.id===-1?<Navigate to="/login"/>:<Create/>} />
+        <Route path="/account" element={user.id===-1?<Navigate to="/login"/>:<Account/>} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
